1365bfe752ae5bf4d27fc6807cc1af5d4a1d1990
hooman777 <eng.hesamyan@gmail.com>
2019-11-19 13:30:10 +0400

    uploading build folder

---
+++ build/precache-manifest.a926aeb72cbe14ce6bc9ad50c4a7483b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627ae85312d4d559ffaf4f2b9af4b9e4",
+    "url": "/video_player/index.html"
+  },
+  {
+    "revision": "3652a901ed3d2be284f1",
+    "url": "/video_player/static/css/main.93eb4199.chunk.css"
+  },
+  {
+    "revision": "c86fecbb37dac78d7887",
+    "url": "/video_player/static/js/2.b2bd3bbe.chunk.js"
+  },
+  {
+    "revision": "3652a901ed3d2be284f1",
+    "url": "/video_player/static/js/main.c6c7ba88.chunk.js"
+  },
+  {
+    "revision": "4769ce247cf0bd294e0f",
+    "url": "/video_player/static/js/runtime-main.f6baccef.js"
+  }
+]);
